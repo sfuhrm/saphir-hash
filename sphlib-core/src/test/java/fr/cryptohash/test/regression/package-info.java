58bc3d40bcbf3a4f1c88c486a43d6c061d541f38
Stephan Fuhrmann <s_fuhrm@gmx.de>
2014-05-02 12:18:02 +0200

    NetBeans reformatting for machine generated code.

--- sphlib-core/src/test/java/fr/cryptohash/test/regression/package-info.java
+++ sphlib-core/src/test/java/fr/cryptohash/test/regression/package-info.java
@@ -1,12 +1,13 @@
 // $Id: package-info.java 157 2010-04-26 19:03:44Z tp $
-
 /**
- * <p>The {@code fr.cryptohash.test.regression} package contains for
- * many algorithms some example data.</p>
+ * <p>
+ * The {@code fr.cryptohash.test.regression} package contains for many
+ * algorithms some example data.</p>
  *
- * <p>The files were generated from the original <code>TestDigest.java</code> file.</p>
+ * <p>
+ * The files were generated from the original <code>TestDigest.java</code>
+ * file.</p>
  *
- * @author    Stephan Fuhrmann
+ * @author Stephan Fuhrmann
  */
-
 package fr.cryptohash.test.regression;
