62216a0392832b716199de0f201a159d24cff182
Stephan Fuhrmann <s_fuhrm@gmx.de>
2014-05-02 17:53:31 +0200

    Big rename

--- saphir-hash-speed/src/main/java/de/tynne/sphlib/SpeedTest.java
+++ saphir-hash-speed/src/main/java/de/tynne/sphlib/SpeedTest.java
@@ -1,36 +1,11 @@
-/*
- * Copyright (c) 2014, Stephan Fuhrmann <stephan@tynne.de>
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- * * Redistributions of source code must retain the above copyright notice, this
- *   list of conditions and the following disclaimer.
- * * Redistributions in binary form must reproduce the above copyright notice,
- *   this list of conditions and the following disclaimer in the documentation
- *   and/or other materials provided with the distribution.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- */
-package fr.cryptohash.test.speed;
+package de.tynne.sphlib;
+
 
 import fr.cryptohash.JCAProvider;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.Provider;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
-import org.junit.Test;
 
 /**
  * Compares speed of SH-Lib, Bouncy castle and SUN providers.
@@ -72,15 +47,20 @@ public class SpeedTest {
         new TestSpec(100*K*K, "100M"),
     };
     
-    @Test
-    public void testSpeed() throws NoSuchAlgorithmException {
+    public static void main(String args[]) throws NoSuchAlgorithmException {
         JCAProvider sphlib = new JCAProvider();
         BouncyCastleProvider bouncy = new BouncyCastleProvider();
 
         // get SUN provider by getting the MD5 digest as an example
         MessageDigest md5 = MessageDigest.getInstance("MD5");
 
-        Provider providers[] = new Provider[]{md5.getProvider(), sphlib, bouncy};
+        Provider providers[];
+        
+        if (args.length == 0) {
+            providers = new Provider[]{md5.getProvider(), sphlib, bouncy};
+        } else {
+            providers = new Provider[]{sphlib};
+        }
 
         System.out.println("Algo,Size,SunProvider,SunDuration,SphProvider,SphDuration,BcProvider,SphDuration");
         
@@ -110,7 +90,7 @@ public class SpeedTest {
      * @param verbose whether to output the test result to  {@code System.out}.
      * @param first is this the first test for this algo? Used to format the output.
      */
-    private void test(String algo, TestSpec spec, MessageDigest md, boolean verbose, boolean first) {
+    private static void test(String algo, TestSpec spec, MessageDigest md, boolean verbose, boolean first) {
         byte oneK[] = new byte[K];
 
         long start = System.nanoTime();
