8b16d51ac7c11c274a7676f55008d95901ed5eb4
Stephan Fuhrmann <s_fuhrm@gmx.de>
2014-05-02 19:28:45 +0200

    Cleaning up a bit

---
+++ saphir-hash-speed/src/main/java/de/tynne/sphlib/SizeWithMultiplier.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright (c) 2014, Stephan Fuhrmann <stephan@tynne.de>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package de.tynne.sphlib;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Number with multiplier, like "5 M".
+ * @author Stephan Fuhrmann <stephan@tynne.de>
+ */
+public class SizeWithMultiplier {
+    
+    private final long base;
+    private final Multiplier multiplier;
+
+    public SizeWithMultiplier(long base, Multiplier multiplier) {
+        this.base = base;
+        this.multiplier = multiplier;
+    }
+
+    public long getBase() {
+        return base;
+    }
+
+    public Multiplier getMultiplier() {
+        return multiplier;
+    }
+    
+    public long getNumber() {
+        return base * multiplier.multiplier();
+    }
+    
+    public static SizeWithMultiplier parseString(String in) {
+        Pattern p = Pattern.compile("(-?[0-9]+) *([KMGT])");
+        Matcher m = p.matcher(in);
+        if (m.matches()) {
+            long num = Long.parseLong(m.group(1));
+            Multiplier unit = Multiplier.valueOf(m.group(2));
+            return new SizeWithMultiplier(num, unit);
+        }
+        throw new IllegalArgumentException("Unparseable: "+in);
+    }
+    
+    @Override
+    public String toString() {
+        return Long.toString(base) + multiplier;
+    }
+}
--- saphir-hash-speed/src/main/java/de/tynne/sphlib/SpeedTest.java
+++ saphir-hash-speed/src/main/java/de/tynne/sphlib/SpeedTest.java
@@ -1,10 +1,38 @@
+/*
+ * Copyright (c) 2014, Stephan Fuhrmann <stephan@tynne.de>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
 package de.tynne.sphlib;
 
 
 import fr.cryptohash.JCAProvider;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
 import java.security.Provider;
+import java.util.HashMap;
+import java.util.Map;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 
 /**
@@ -15,16 +43,18 @@ import org.bouncycastle.jce.provider.BouncyCastleProvider;
  */
 public class SpeedTest {
 
-    /** These algorithms are also in the standard JDK. */
-    private final static String ALGORITHM_NAMES[] = new String[]{
-        "MD2",
-        "MD5",
-        "SHA",
-        "SHA-224",
-        "SHA-256",
-        "SHA-384",
-        "SHA-512"
-    };
+    private final Arguments arguments;
+    private final Map<String, Provider> providers;
+
+    public SpeedTest(Arguments arguments) throws NoSuchAlgorithmException {
+        this.arguments = arguments;
+        
+        providers = new HashMap<String, Provider>();
+        providers.put("BC", new BouncyCastleProvider());
+        providers.put("SPH", new JCAProvider());
+        providers.put("SUN", MessageDigest.getInstance("MD5").getProvider());
+        
+    }    
     
     private static class TestSpec {
         long byteSize;
@@ -35,41 +65,34 @@ public class SpeedTest {
             this.name = name;
         }
     }
-
-    /** Kilobyte. */
-    private final static int K = 1024;
     
-    private final static TestSpec TESTSPECS[] = new TestSpec[] {
-        new TestSpec(K, "1K"),
-        new TestSpec(10*K, "10K"),
-        new TestSpec(100*K, "100K"),
-        new TestSpec(K*K, "1M"),
-        new TestSpec(100*K*K, "100M"),
-    };
-    
-    public static void main(String args[]) throws NoSuchAlgorithmException {
-        JCAProvider sphlib = new JCAProvider();
-        BouncyCastleProvider bouncy = new BouncyCastleProvider();
-
-        // get SUN provider by getting the MD5 digest as an example
-        MessageDigest md5 = MessageDigest.getInstance("MD5");
-
-        Provider providers[];
+    public static void main(String args[]) throws NoSuchAlgorithmException, NoSuchProviderException {
         
-        if (args.length == 0) {
-            providers = new Provider[]{md5.getProvider(), sphlib, bouncy};
-        } else {
-            providers = new Provider[]{sphlib};
+        Arguments cliArgs = Arguments.parse(args);
+        if (cliArgs == null) {
+            return;
         }
-
-        System.out.println("Algo,Size,SunProvider,SunDuration,SphProvider,SphDuration,BcProvider,SphDuration");
         
-        for (String algo : ALGORITHM_NAMES) {
+        SpeedTest test = new SpeedTest(cliArgs);
+        test.speedTest();
+    }
 
-            for (TestSpec spec : TESTSPECS) {
+    private void speedTest() throws NoSuchAlgorithmException, NoSuchProviderException {
+        System.out.print("Algo,Size");
+        for (String prov : arguments.getProviders())
+            System.out.print(","+prov+"Name,"+prov+"TimeMillis");
+        System.out.println();
+        
+        for (String algo : arguments.getAlgorithms()) {
+
+            for (SizeWithMultiplier spec : arguments.getSizes()) {
                 boolean first = true;
-                for (Provider provider : providers) {
-                    MessageDigest messageDigest = MessageDigest.getInstance(algo, provider);
+                
+                for (String provider : arguments.getProviders()) {
+                    
+                    Provider providerInstance = providers.get(provider);
+                    
+                    MessageDigest messageDigest = MessageDigest.getInstance(algo, providerInstance);
                     // once without output to hopefully JIT compile
                     test(algo, spec, messageDigest, false, first);
                     // once with output
@@ -90,12 +113,12 @@ public class SpeedTest {
      * @param verbose whether to output the test result to  {@code System.out}.
      * @param first is this the first test for this algo? Used to format the output.
      */
-    private static void test(String algo, TestSpec spec, MessageDigest md, boolean verbose, boolean first) {
-        byte oneK[] = new byte[K];
+    private static void test(String algo, SizeWithMultiplier spec, MessageDigest md, boolean verbose, boolean first) {
+        byte oneK[] = new byte[1024];
 
         long start = System.nanoTime();
 
-        for (int count = 0; count < spec.byteSize; count += oneK.length) {
+        for (int count = 0; count < spec.getNumber(); count += oneK.length) {
             md.update(oneK);
         }
         md.digest();
@@ -104,7 +127,7 @@ public class SpeedTest {
 
         if (verbose) {
             if (first) {
-                System.out.print(algo + "," + spec.name + "," + md.getProvider().getName() + ","  + (end - start));
+                System.out.print(algo + "," + spec + "," + md.getProvider().getName() + ","  + (end - start));
             } else {
                 System.out.print(","+md.getProvider().getName() + ","  + (end - start));
             }
