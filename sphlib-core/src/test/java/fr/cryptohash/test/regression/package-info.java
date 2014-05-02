d4a5ec0ca5aae4fca82b9d16e96f1b56c4304490
Stephan Fuhrmann <s_fuhrm@gmx.de>
2014-05-02 12:21:21 +0200

    License headers

--- sphlib-core/src/test/java/fr/cryptohash/test/regression/package-info.java
+++ sphlib-core/src/test/java/fr/cryptohash/test/regression/package-info.java
@@ -1,4 +1,28 @@
-// $Id: package-info.java 157 2010-04-26 19:03:44Z tp $
+/*
+ * Copyright (c) 2014, Stephan Fuhrmann <stephan@tynne.de>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
 /**
  * <p>
  * The {@code fr.cryptohash.test.regression} package contains for many
@@ -8,6 +32,6 @@
  * The files were generated from the original <code>TestDigest.java</code>
  * file.</p>
  *
- * @author Stephan Fuhrmann
+ * @author Stephan Fuhrmann <stephan@tynne.de>
  */
 package fr.cryptohash.test.regression;
