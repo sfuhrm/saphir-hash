0f1dced2da7a8c92794d77eaf4c33a89d5c57448
Stephan Fuhrmann <s_fuhrm@gmx.de>
2014-05-02 12:56:33 +0200

    Moving of package Deleting the sun names one shot

--- sphlib-speed/src/test/java/fr/cryptohash/test/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * <p>The {@code fr.cryptohash.test} package contains some test code
- * to test the JCA provider.</p>
- * @author Stephan Fuhrmann
- */
-
-package fr.cryptohash.test;
--- sphlib-speed/src/test/java/fr/cryptohash/test/speed/SpeedTest.java
+++ sphlib-speed/src/test/java/fr/cryptohash/test/speed/SpeedTest.java
@@ -1,9 +1,29 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright (c) 2014, Stephan Fuhrmann <stephan@tynne.de>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
  */
-package fr.cryptohash.test;
+package fr.cryptohash.test.speed;
 
 import fr.cryptohash.JCAProvider;
 import java.security.MessageDigest;
@@ -20,6 +40,7 @@ import org.junit.Test;
  */
 public class SpeedTest {
 
+    /** These algorithms are also in the standard JDK. */
     private final static String ALGORITHM_NAMES[] = new String[]{
         "MD2",
         "MD5",
@@ -30,8 +51,13 @@ public class SpeedTest {
         "SHA-512"
     };
 
+    /** Kilobyte. */
     private final static int K = 1024;
+    
+    /** With what sizes to test. */
     public final static int TEST_SIZES[] = new int[]{K, 10 * K, 100 * K, K * K, 100 * K * K};
+    
+    /** The human readable test size names. */
     public final static String TEST_NAMES[] = new String[]{"1K", "10K", "100K", "1M", "100M"};
 
     @Test
@@ -39,6 +65,7 @@ public class SpeedTest {
         JCAProvider sphlib = new JCAProvider();
         BouncyCastleProvider bouncy = new BouncyCastleProvider();
 
+        // get SUN provider by getting the MD5 digest as an example
         MessageDigest md5 = MessageDigest.getInstance("MD5");
 
         Provider providers[] = new Provider[]{md5.getProvider(), sphlib, bouncy};
@@ -62,6 +89,13 @@ public class SpeedTest {
         }
     }
 
+    /** Speed test for one MD algo.
+     * @param algo the algorithm name.
+     * @param size the byte size of the test data to digest.
+     * @param md the algorithm instance to use.
+     * @param verbose whether to output the test result to  {@code System.out}.
+     * @param first is this the first test for this algo? Used to format the output.
+     */
     private void test(String algo, int size, MessageDigest md, boolean verbose, boolean first) {
         byte oneK[] = new byte[K];
 
